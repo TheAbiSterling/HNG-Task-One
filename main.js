72a078239fc65b76d7c88cacaf3a9638843d1ffb
The Abi <137959447+TheAbiSterling@users.noreply.github.com>
2023-09-07 11:58:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function updateDateTime() {
+  const dayOfWeek = new Date().toLocaleDateString("en-US", { weekday: "long" });
+  const utcTime = new Date().toUTCString();
+
+  document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+    dayOfWeek;
+  document.querySelector('[data-testid="currentUTCTime"]').textContent =
+    utcTime;
+}
+
+setInterval(updateDateTime, 1000); // Update every second
