a09722ea6797e33cdb9ecad7fbb59447e9a532f2
The Abi <137959447+TheAbiSterling@users.noreply.github.com>
2023-09-08 10:32:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function updateDateTime() {
   const dayOfWeek = new Date().toLocaleDateString("en-US", { weekday: "long" });
+  const utcDate = new Date().toUTCString();
+
+  // Extract just the time from the UTC string
   const utcTime = new Date().toUTCString();
 
   document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
